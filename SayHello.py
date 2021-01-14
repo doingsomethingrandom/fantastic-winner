1a0f0dea54f869f34cd384d81c8e5c5e3dc8da82
Tharun <mygithubupdate@gmail.com>
2021-01-14 15:33:12 +0000

    Update and rename SayHello.ipynb.json to SayHello.py

---
+++ SayHello.py
@@ -0,0 +1,11 @@
+import random
+age = random.randrange(0, 100)
+fname = "Tharun"
+lname = "Pandiyan"
+fullName = f"{fname} {lname}"
+greet = f"Hello {fullName}"
+print(f"""{fname} is {age}-years-old.
+His last name is {lname}.
+He's {age}-year-old.
+Say hello to him.
+{greet}.""")
